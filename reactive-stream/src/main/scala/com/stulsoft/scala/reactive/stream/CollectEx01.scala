37d1af496a044930ad5ca1835a90eb67dc0c5b30
Yuriy Stul <ysden123@gmail.com>
2018-12-08 15:43:19 +0200

    Fold, collect, and drop

---
+++ reactive-stream/src/main/scala/com/stulsoft/scala/reactive/stream/CollectEx01.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.scala.reactive.stream
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.typesafe.scalalogging.LazyLogging
+
+/** Use of collect
+  *
+  * @author Yuriy Stul
+  */
+object CollectEx01 extends App with LazyLogging {
+  implicit val actorSystem: ActorSystem = ActorSystem("NumberSystem")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val numbersSource = Source(1 to 10)
+
+  val sampleSink = Sink.foreach((n: Int) => logger.info(n.toString))
+
+  val numberFlow = Flow[Int].map(num => num + 1).collect{case num2 => num2 * 3}
+
+  val numberRunnableGraph =
+    numbersSource.via(numberFlow).to(sampleSink)
+
+  numberRunnableGraph.run
+
+  actorSystem.terminate
+}
